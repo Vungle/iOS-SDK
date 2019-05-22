6d67c94d4ac8406321874dab90ab71628ba0efc4
Mourad <mourad-meddeb>
2019-05-22 11:01:33 +0100

    Added new files

---
+++ Sample/Vungle Sample App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vungle Sample App
+//
+//  Created by Vungle on 3/24/14.
+//  Copyright (c) 2017 Vungle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
