b4b1d5c1a5414114d7bce021a87887bbc1f2aadc
johnmaivungle <john.mai@vungle.com>
2020-06-15 13:41:56 -0700

    Took out unused methods

--- Sample/Vungle Sample App/MRECTableViewCell.m
+++ Sample/Vungle Sample App/MRECTableViewCell.m
@@ -8,17 +8,10 @@
 
 #import "MRECTableViewCell.h"
 
+@interface MRECTableViewCell()
+
+@end
+
 @implementation MRECTableViewCell
-
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
 
 @end
