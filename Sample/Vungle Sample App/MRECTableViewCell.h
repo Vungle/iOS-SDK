8953c12d6ad41528cf9674ee6a3267dfe30cc43e
johnmaivungle <john.mai@vungle.com>
2020-06-17 15:25:43 -0700

    Added labels for the placementIDs and ad type

--- Sample/Vungle Sample App/MRECTableViewCell.h
+++ Sample/Vungle Sample App/MRECTableViewCell.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UIButton *loadMRECButton;
 @property (weak, nonatomic) IBOutlet UIButton *playMRECButton;
 @property (weak, nonatomic) IBOutlet UIButton *dismissMRECButton;
+@property (weak, nonatomic) IBOutlet UILabel *mrecLabel;
 @property (retain, nonatomic) IBOutlet UIView *mrecView;
 
 @end
